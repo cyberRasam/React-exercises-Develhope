4f0a47566b868312350658bb574192fc7832fc4f
cyberrasam <rassamrl@gmail.com>
2023-03-11 01:14:41 +0300

    components 07 exc

--- my-app/src/components/Hello.jsx
+++ my-app/src/components/Hello.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Message } from './Message';
 
 export class Hello extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello, World!</h1>
-        <p>What a beautiful day!</p>
+        <Message />
       </div>
     );
   }
