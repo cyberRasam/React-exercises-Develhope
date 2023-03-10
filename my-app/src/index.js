e42ce13f45a1aee8a2ab35346a5f4c422bd87cb4
cyberrasam <rassamrl@gmail.com>
2023-03-11 01:08:42 +0300

    components 04 exc

---
+++ my-app/src/index.js
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './components/App';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />);
+
+//ReactDOM.render method is no longer supported so i used createRoot instead
