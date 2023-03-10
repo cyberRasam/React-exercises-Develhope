1bebae96591b153abb8d769a4d9716f4fc76df86
cyberrasam <rassamrl@gmail.com>
2023-03-11 00:58:14 +0300

    components 03 exc

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hello } from './Hello';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    );
+  }
+}
