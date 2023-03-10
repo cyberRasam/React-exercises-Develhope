6d4e4b303a175a4c161adc626a7d747f6795e101
cyberrasam <rassamrl@gmail.com>
2023-03-11 01:17:46 +0300

    props 01 exc

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Hello } from './Hello';
+import { Welcome } from './Welcome';
 
 export class App extends React.Component {
   render() {
     return (
       <div>
         <Hello />
+        <Welcome name="Adrian" />
       </div>
     );
   }
