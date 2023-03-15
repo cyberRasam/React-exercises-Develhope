af398f1daa1319c8118e7f4184efa77d8b432a7c
cyberrasam <rassamrl@gmail.com>
2023-03-15 14:37:35 +0300

    conditional rendering 05

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -8,7 +8,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <Welcome name="John" age={18} />
+        <Welcome name="John" age={19} />
         <Clock></Clock>
       </div>
     );
