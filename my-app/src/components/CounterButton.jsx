bd5fecd9dd20411f5f20a17937c6826f7d744670
cyberrasam <rassamrl@gmail.com>
2023-03-29 14:55:55 +0300

    exc done

--- my-app/src/components/CounterButton.jsx
+++ my-app/src/components/CounterButton.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
 
 class CounterButton extends React.Component {
   render() {
-    return <button onClick={this.props.onClick}>Increment</button>;
+    return <Button className="text-capitalize" onClick={this.props.onClick}>Increment</Button>;
   }
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import './index.css'
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = createRoot(document.getElementById('root'));
 
