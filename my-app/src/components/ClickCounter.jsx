c6dd63c58fb3fd52b7e8f22633e21a48e96fe6de
cyberrasam <rassamrl@gmail.com>
2023-03-31 20:49:16 +0300

    useEffect-01 exc done

--- my-app/src/components/ClickCounter.jsx
+++ my-app/src/components/ClickCounter.jsx
@@ -1,11 +1,18 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import CounterButton from './CounterButton';
 
 export const ClickCounter = () => {
   const [count, setCount] = useState(0)
   function handleCounter() {
     setCount (count + 1)
+  
   }
+  function onCounterChange(){
+    console.log(`current counter is ${count}`)
+  }
+  useEffect(()=>{
+    onCounterChange()
+  },[count])
     return (
       <div>
         <h1>{count}</h1>
--- my-app/src/components/ToDoList.jsx
+++ my-app/src/components/ToDoList.jsx
@@ -1,70 +1,77 @@
 import React, { Component } from 'react'
 
-export class ToDoList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            items: [],
-            todo : ''
-        }
-    }
+// export class ToDoList extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             items: [],
+//             todo : ''
+//         }
+//     }
 
 
-    handleInputChange = (event) => {
-        const todo = event.target.value;
-        this.setState({
-            todo: todo
-        })
-    }
+//     handleInputChange = (event) => {
+//         const todo = event.target.value;
+//         this.setState({
+//             todo: todo
+//         })
+//     }
 
-    clickHandler = (e) => {
-        e.preventDefault()
-        const todo = this.state.todo
-        this.setState({
-            items: [...this.state.items, todo],
-            todo: ''
-        })
-    }
+//     clickHandler = (e) => {
+//         e.preventDefault()
+//         const todo = this.state.todo
+//         this.setState({
+//             items: [...this.state.items, todo],
+//             todo: ''
+//         })
+//     }
 
 
-    resetHandler = (e) => {
-      this.setState({
-        items: []
-      })
-    }
+//     resetHandler = (e) => {
+//       this.setState({
+//         items: []
+//       })
+//     }
 
-    handleRemove = (index) => {
-      const items = [...this.state.items]
-      items.splice(index, 1)
+//     handleRemove = (index) => {
+//       const items = [...this.state.items]
+//       items.splice(index, 1)
 
-      this.setState({
-        items: items
-      })
-    } 
+//       this.setState({
+//         items: items
+//       })
+//     } 
 
-  render() {
+//   render() {
     
-    const {items} = this.state
-    return (
-      <div>
-        <input type="text" name="toDo" value={this.state.todo} onChange={this.handleInputChange} />
-        <button type='button' onClick={this.clickHandler}>Add ToDo</button> 
-        <button type='button' onClick={this.resetHandler}>Reset</button>
-        {/* <ul>
-            {this.state.items.map(
-            (element, index) => 
-              <li key={index}>
-                {element}
-                <button type='button' onClick={this.handleRemove}>Remove</button>
-              </li>
-              )
-            }
-        </ul> */}
-        {this.props.children(items, this.handleRemove)}
+//     const {items} = this.state
+//     return (
+//       <div>
+//         <input type="text" name="toDo" value={this.state.todo} onChange={this.handleInputChange} />
+//         <button type='button' onClick={this.clickHandler}>Add ToDo</button> 
+//         <button type='button' onClick={this.resetHandler}>Reset</button>
+//         {/* <ul>
+//             {this.state.items.map(
+//             (element, index) => 
+//               <li key={index}>
+//                 {element}
+//                 <button type='button' onClick={this.handleRemove}>Remove</button>
+//               </li>
+//               )
+//             }
+//         </ul> */}
+//         {this.props.children(items, this.handleRemove)}
 
-      </div>
-    )
-  }
+//       </div>
+//     )
+//   }
+// }
+
+export const ToDoList = () => {
+  return (
+    <div></div>
+  )
 }
+
 
 export default ToDoList
