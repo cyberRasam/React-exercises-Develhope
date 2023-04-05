3c6bb207351668fc828fbd52c68b2df817a0934c
cyberrasam <rassamrl@gmail.com>
2023-04-05 20:19:30 +0300

    custom-Hook-01

---
+++ my-app/src/components/useCounter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export function useCounter(initialValue = 0) {
+    const [count, setCount] = useState(initialValue);
+
+    function increment(){
+        setCount(c => c+1)
+    }
+
+    function decrement(){
+        setCount(c => c-1)
+    }
+
+    function reset() {
+        setCount(initialValue)
+    }
+
+    return [count, increment, decrement, reset]
+}
