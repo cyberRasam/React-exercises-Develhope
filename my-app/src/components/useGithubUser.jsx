981089348836cebe74f7fd39aba15da32f4c029f
cyberrasam <rassamrl@gmail.com>
2023-04-09 19:18:10 +0300

    swr-2

--- my-app/src/components/useGithubUser.jsx
+++ my-app/src/components/useGithubUser.jsx
@@ -4,8 +4,13 @@ import useSWR from 'swr'
 const fetcher = url => fetch(url).then(res => res.json())
 
 export default function useGithubUser(username) {
+  
   const {data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
-
+  
+  if (!username) {
+    return null;
+  }
+  
   return { users:data,
      error,
      isLoading: !data && !error };
